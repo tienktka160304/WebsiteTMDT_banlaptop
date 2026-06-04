6f3e5bced2f6615a5f1ce7283bfa5a7581177d96
tienktka160304 <nvtien163@Gmail.com>
2026-06-04 09:02:38 +0700

    Update vite.config.js

--- client/React-client/vite.config.js
+++ client/React-client/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "./",
-  build: {
-    outDir: "public/build", // xuất ra trong thư mục public
-    emptyOutDir: true, // xóa thư mục trước khi build
-  },
 });
